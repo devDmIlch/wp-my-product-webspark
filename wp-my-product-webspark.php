d7fc196b42c7918802a73247f555aed74e9e73b3
devDmIlch <dm.ilch.mail@gmail.com>
2025-02-21 12:33:52 +0200

    [General]  - Added initial file of the plugin.

---
+++ wp-my-product-webspark.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: WebSpark WooCommerce Plugin
+ * Plugin URI: https://github.com/devDmIlch/wp-my-product-webspark
+ * Description: test plugin
+ * Author: Dmitrii Ilchenko
+ * Version: 0.0.1
+ * Author URI: TG: @chSnake
+ */
